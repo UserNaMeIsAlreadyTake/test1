fd6da04931c00e55cbba9e0fa13c472554f30ee8
simon <simon-hiller@gmx.net>
2022-03-27 13:15:17 +0200

    test

--- test1/test.cpp
+++ test1/test.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <limits>
+
+
 using namespace std;
 
-int main(int argc, char const *argv[])
+int imGlobal = 0;
+const  double PI = 3.1415;
+
+
+int main()
 {
-    std::cout  << "Hello world" << endl;
+    cout << "Hello world" << endl;
+   
     return 0;
 }
